3bd8982f67e33af99b0e60cdd431cf8c6b7003f1
Hussein Elali <shan_elali@hotmail.com>
2025-07-03 15:10:47 +0200

    Add utils.py with unique API key generator, fix signals.py import

--- ai_assistant/accounts/signals.py
+++ ai_assistant/accounts/signals.py
@@ -2,12 +2,15 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import UserProfile
+from .utils import generate_unique_api_key
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        profile = UserProfile.objects.create(user=instance)
-        profile.generate_api_key()  # generate API key immediately after profile creation
+        UserProfile.objects.create(
+            user=instance,
+            api_key=generate_unique_api_key()
+        )
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
