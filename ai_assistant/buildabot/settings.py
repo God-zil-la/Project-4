c0b1e1c02c85ded182022eb84a1a42a1b273528b
Hussein Elali <shan_elali@hotmail.com>
2025-07-04 17:52:20 +0200

    set debug false

--- ai_assistant/buildabot/settings.py
+++ ai_assistant/buildabot/settings.py
@@ -18,7 +18,7 @@ ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=ce
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-placeholder")
 DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("true", "1")
 
-DEBUG = True # must be True during local dev to allow ngrok host
+DEBUG = False # must be True during local dev to allow ngrok host
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
