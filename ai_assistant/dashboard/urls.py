56d1c522972f2fd700a9022b77cdc65f3758a840
Mr.Husse <shan_elali@hotmail.com>
2025-07-28 21:47:33 +0200

    Update urls.py adding error

--- ai_assistant/dashboard/urls.py
+++ ai_assistant/dashboard/urls.py
@@ -5,4 +5,9 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    # Test error views
+    path('test400/', views.trigger_400, name='test-400'),
+    path('test403/', views.trigger_403, name='test-403'),
+    path('test500/', views.trigger_500, name='test-500'),
 ]
