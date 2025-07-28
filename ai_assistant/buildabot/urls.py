37846e194720899a29ba0db103cc8934ab679d74
Mr.Husse <shan_elali@hotmail.com>
2025-07-28 21:24:54 +0200

    Update urls.py adding error pages

--- ai_assistant/buildabot/urls.py
+++ ai_assistant/buildabot/urls.py
@@ -16,6 +16,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Error handlers
+handler400 = "django.views.defaults.bad_request"
+handler403 = "django.views.defaults.permission_denied"
+handler404 = "django.views.defaults.page_not_found"
+handler500 = "django.views.defaults.server_error"
