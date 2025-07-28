b89f78ed0d910559254d0e7ab6d744b3df8495b9
Mr.Husse <shan_elali@hotmail.com>
2025-07-28 16:37:57 +0200

    Update apps.py add docstrings

--- ai_assistant/accounts/apps.py
+++ ai_assistant/accounts/apps.py
@@ -2,8 +2,11 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    """Configuration for the accounts app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai_assistant.accounts'
 
     def ready(self):
+        """Import signal handlers when the app is ready."""
         import ai_assistant.accounts.signals
